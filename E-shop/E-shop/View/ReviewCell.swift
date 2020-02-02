4f9efca7ad9263da92a08acb6ccc589e796ab314
Evo4 <pro100kenny1@gmail.com>
2020-02-02 19:28:11 +0200

    Added ReviewCell

---
+++ E-shop/E-shop/View/ReviewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewCell.swift
+//  E-shop
+//
+//  Created by Vyacheslav on 02.02.2020.
+//  Copyright © 2020 Vyacheslav. All rights reserved.
+//
+
+import UIKit
+
+class ReviewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
