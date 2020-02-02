45d09f377ec22c6b91c702e309f14cff0d3fd250
Evo4 <pro100kenny1@gmail.com>
2020-02-02 18:19:55 +0200

    Added NewReview view controller

---
+++ E-shop/E-shop/Controller/NewReviewVC.swift
@@ -0,0 +1,19 @@
+//
+//  NewReviewVC.swift
+//  E-shop
+//
+//  Created by Vyacheslav on 02.02.2020.
+//  Copyright © 2020 Vyacheslav. All rights reserved.
+//
+
+import UIKit
+
+class NewReviewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
