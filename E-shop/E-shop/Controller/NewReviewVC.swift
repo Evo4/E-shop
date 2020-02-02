6f90e4b564280d9737815335afd922fb32e78dfc
Evo4 <pro100kenny1@gmail.com>
2020-02-02 18:25:30 +0200

    Shifted adding new review from ProductCardView to NewReviewVC

--- E-shop/E-shop/Controller/NewReviewVC.swift
+++ E-shop/E-shop/Controller/NewReviewVC.swift
@@ -10,10 +10,67 @@ import UIKit
 
 class NewReviewVC: UIViewController {
 
+    let rateButons:[UIButton] = [UIButton(), UIButton(), UIButton(), UIButton(), UIButton()]
+    
+    private lazy var rateStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: self.rateButons)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    private lazy var reviewLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Raleway-Bold", size: 17)
+        label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        label.text = "Review:"
+        return label
+    }()
+    
+    private lazy var reviewTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        textView.font = UIFont(name: "Raleway-Regular", size: 17)
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
+    func setupConstraints() {
+        [rateStackView, reviewLabel, reviewTextView].forEach { (subview) in
+            view.addSubview(subview)
+        }
+        NSLayoutConstraint.activate([
+            
+            rateStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            rateStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            rateStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            rateStackView.heightAnchor.constraint(equalToConstant: 40),
+            
+            reviewLabel.topAnchor.constraint(equalTo: rateStackView.bottomAnchor, constant: 10),
+            reviewLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            
+            reviewTextView.topAnchor.constraint(equalTo: reviewLabel.bottomAnchor, constant: 4),
+            reviewTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            reviewTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            reviewTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
+        ])
+    }
+    
+    func setupRateButtons() {
+           rateButons.forEach { (button) in
+               button.setImage(#imageLiteral(resourceName: "star"), for: .normal)
+               NSLayoutConstraint.activate([
+                   button.widthAnchor.constraint(equalToConstant: 40),
+               ])
+           }
+       }
 }
--- E-shop/E-shop/View/ProductCardView.swift
+++ E-shop/E-shop/View/ProductCardView.swift
@@ -28,33 +28,6 @@ class ProductCardView: UIView {
         return label
     }()
     
-    let rateButons:[UIButton] = [UIButton(), UIButton(), UIButton(), UIButton(), UIButton()]
-    
-    private lazy var rateStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: self.rateButons)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.spacing = 5
-        return stackView
-    }()
-    
-    private lazy var reviewLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont(name: "Raleway-Bold", size: 17)
-        label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        label.text = "Review:"
-        return label
-    }()
-    
-    private lazy var reviewTextView: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        textView.font = UIFont(name: "Raleway-Regular", size: 17)
-        textView.layer.borderWidth = 1
-        textView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        return textView
-    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -73,11 +46,10 @@ class ProductCardView: UIView {
         self.layer.shadowOffset = CGSize(width: 0, height: 0)
         self.layer.shadowRadius = 15
         self.layer.shadowOpacity = 0.25
-        setupRateButtons()
     }
     
     func setupConstraints() {
-        [titleLabel, descriptionLabel, rateStackView, reviewLabel, reviewTextView].forEach { (subview) in
+        [titleLabel, descriptionLabel].forEach { (subview) in
             self.addSubview(subview)
         }
         NSLayoutConstraint.activate([
@@ -86,28 +58,8 @@ class ProductCardView: UIView {
             
             descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 7),
             descriptionLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20),
-            
-            rateStackView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 15),
-            rateStackView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20),
-            rateStackView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20),
-            rateStackView.heightAnchor.constraint(equalToConstant: 40),
-            
-            reviewLabel.topAnchor.constraint(equalTo: rateStackView.bottomAnchor, constant: 10),
-            reviewLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20),
-            
-            reviewTextView.topAnchor.constraint(equalTo: reviewLabel.bottomAnchor, constant: 4),
-            reviewTextView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20),
-            reviewTextView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20),
-            reviewTextView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
         ])
     }
     
-    func setupRateButtons() {
-        rateButons.forEach { (button) in
-            button.setImage(#imageLiteral(resourceName: "star"), for: .normal)
-            NSLayoutConstraint.activate([
-                button.widthAnchor.constraint(equalToConstant: 40),
-            ])
-        }
-    }
+   
 }
