e67d3e435ec03012fe6cbc0efac44bca54e942de
Evo4 <pro100kenny1@gmail.com>
2020-02-06 01:03:36 +0200

    Added SettingsVC

---
+++ E-shop/E-shop/Controller/SettingsVC.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsVC.swift
+//  E-shop
+//
+//  Created by Vyacheslav on 06.02.2020.
+//  Copyright © 2020 Vyacheslav. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
